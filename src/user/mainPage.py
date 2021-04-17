d350b61da0c837b3e5bf8822d21af5541e894616
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-17 18:03:41 +0700

    minor changes on gui

--- src/user/mainPage.py
+++ src/user/mainPage.py
@@ -12,19 +12,19 @@ class mainPage(tk.Frame):
         self.main_page()
 
     def main_page(self):
-        template.header("Halaman Utama")
+        template.header("halaman utama")
         template.button_film(self)
         template.button_snack(self)
-        fontStyle = tkFont.Font(size = 13)
+        fontStyle = tkFont.Font(family="TimeBurner", size = 13)
         
         self.sign_in = tk.Button(text="Sign In", font=fontStyle, command=lambda: template.changePage(self, "sign in"))
         self.sign_in.place(x=625, y=200, width=140, height=50, anchor="c")
 
-        self.sign_up = tk.Button(text="Sign Up", font=fontStyle)
+        self.sign_up = tk.Button(text="Sign Up", font=fontStyle, command=lambda: template.changePage(self, "sign up"))
         self.sign_up.place(x=625, y=300, width=140, height=50, anchor="c")
 
-        self.forget = tk.Button(text="Forget Password", font=fontStyle)
-        self.forget.place(x=625, y=400, width=150, height=50, anchor="c")
+        self.forget = tk.Button(text="Forget Password", font=fontStyle, command=lambda: template.changePage(self, "forget password"))
+        self.forget.place(x=625, y=400, width=140, height=50, anchor="c")
 
 def startPage():
     root = tk.Tk()
