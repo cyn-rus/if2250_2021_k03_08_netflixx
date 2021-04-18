d79fd6e6da34673be05b1696fd20a19c883e3a97
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-19 02:09:53 +0700

    better gui sign up

--- src/user/signUp.py
+++ src/user/signUp.py
@@ -16,45 +16,45 @@ class signUp(tk.Frame):
         self.sign_up_page()
 
     def sign_up_page(self):
-        template.header("sign up")
+        template.header("  sign up")
         template.button_halaman_utama(self)
 
-        textFont = tkFont.Font(size=14)
+        textFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
         entryFont = tkFont.Font(size=13)
-        buttonFont = tkFont.Font(size=13)
+        buttonFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
 
-        self.email = tk.Label(text="Email", font=textFont)
+        self.email = tk.Label(text="Email", font=textFont, bg="#24225e", fg="#9f64d8")
         self.email.place(x=610, y=70)
         self.email1 = tk.Entry(font=entryFont)
-        self.email1.place(x=535, y=95, width=210)
+        self.email1.place(x=535, y=95, width=210, height=27)
 
-        self.username = tk.Label(text="Username", font=textFont)
-        self.username.place(x=590, y=130)
+        self.username = tk.Label(text="Username", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.username.place(x=590, y=140)
         self.username1 = tk.Entry(font=entryFont)
-        self.username1.place(x=535, y=155, width=210)
+        self.username1.place(x=535, y=165, width=210, height=27)
 
-        self.pw = tk.Label(text="Password", font=textFont)
-        self.pw.place(x=593, y=190)
+        self.pw = tk.Label(text="Password", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.pw.place(x=593, y=210)
         self.pw1 = tk.Entry(font=entryFont, show="*")
-        self.pw1.place(x=535, y=215, width=210)
+        self.pw1.place(x=535, y=235, width=210, height=27)
 
-        self.conf_pw = tk.Label(text="Konfirmasi Password", font=textFont)
-        self.conf_pw.place(x=550, y=250)
+        self.conf_pw = tk.Label(text="Konfirmasi Password", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.conf_pw.place(x=550, y=280)
         self.conf_pw1 = tk.Entry(font=entryFont, show="*")
-        self.conf_pw1.place(x=535, y=275, width=210)
+        self.conf_pw1.place(x=535, y=305, width=210, height=27)
 
-        self.name = tk.Label(text="Nama", font=textFont)
-        self.name.place(x=613, y=310)
+        self.name = tk.Label(text="Nama", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.name.place(x=610, y=350)
         self.name1 = tk.Entry(font=entryFont)
-        self.name1.place(x=535, y=335, width=210)
+        self.name1.place(x=535, y=375, width=210, height=27)
 
-        self.dob = tk.Label(text="Tanggal Lahir", font=textFont)
-        self.dob.place(x=580, y=370)
-        self.dob1 = tkcalendar.DateEntry(width=31)
-        self.dob1.place(x=535, y=395)
+        self.dob = tk.Label(text="Tanggal Lahir", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.dob.place(x=580, y=420)
+        self.dob1 = tkcalendar.DateEntry(width=31, height=27)
+        self.dob1.place(x=535, y=445)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.isSignUpValid())
-        self.submit.place(x=590, y=480, width=100, height=40)
+        self.submit = tk.Button(text="Submit", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.isSignUpValid())
+        self.submit.place(x=590, y=520, width=100, height=40)
 
     def isSignUpValid(self):
         data = template.readFile("user.csv")
@@ -69,24 +69,24 @@ class signUp(tk.Frame):
         dob = self.dob1.get()
 
         if email == "" or username == "" or pw == "" or conf_pw == "" or name == "":
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
+            tkMessageBox.showwarning("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
             return
 
         emailRegex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
         if not re.search(emailRegex, email):
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Email tidak valid")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Email tidak valid")
             return
         
         if email in listEmail:
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Email sudah terdaftar")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Email sudah terdaftar")
             return
 
         if " " in username:
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Username tidak valid")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Username tidak valid")
             return
 
         if username in listUsername:
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Username sudah terdaftar")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Username sudah terdaftar")
             return
 
         if not checkPassword(pw, conf_pw):
@@ -119,18 +119,19 @@ def checkPassword(pw, conf_pw):
                     ]
 
     if not all(rule(pw) for rule in pwRule):
-        tkMessageBox.showinfo("Netfl\'IXX\'", "Password minimal harus mempunyai panjang 8, memiliki minimal 1 huruf kapital, 1 simbol spesial, dan 1 angka")
+        tkMessageBox.showwarning("Netfl\'IXX\'", "Password minimal memiliki panjang 8, memiliki minimal 1 huruf kapital, 1 simbol spesial, dan 1 angka")
         return False
     
     if pw != conf_pw:
-        tkMessageBox.showinfo("Netfl\'IXX'", "Password dan konfirmasi password tidak sesuai")
+        tkMessageBox.showerror("Netfl\'IXX'", "Password dan konfirmasi password tidak sesuai")
         return False
 
     return True
 
 def startPage():
     root = tk.Tk()
-    root.title("Testing")
+    root.title("Netfl\'IXX\'")
     root.geometry("1920x1080")
+    root.configure(bg="#24225e")
     app = signUp(master = root)
     app.mainloop()
