41aa690bc73cab723f501e63abca22cfd6a446c5
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-19 02:09:38 +0700

    better gui sign in

--- src/user/signIn.py
+++ src/user/signIn.py
@@ -16,30 +16,30 @@ class signIn(tk.Frame):
 
 
     def sign_in_page(self):
-        template.header("sign in")
+        template.header("    sign in")
         template.button_halaman_utama(self)
 
-        textFont = tkFont.Font(size=15)
+        textFont = tkFont.Font(family="TimeBurner", size=15, weight="bold")
         entryFont = tkFont.Font(size=13)
-        buttonFont = tkFont.Font(size=13)
+        buttonFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
 
-        self.email = tk.Label(text="Email/Username", font=textFont)
-        self.email.place(x=577, y=100)
+        self.email = tk.Label(text="Email/Username", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.email.place(x=574, y=100)
         self.email1 = tk.Entry(font=entryFont)
-        self.email1.place(x=540, y=135, width=210, height=27)
+        self.email1.place(x=530, y=135, width=240, height=27)
 
-        self.pw = tk.Label(text="Password", font=textFont)
-        self.pw.place(x=602, y=230)
+        self.pw = tk.Label(text="Password", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.pw.place(x=599, y=230)
         self.pw1 = tk.Entry(font=entryFont, show="*")
-        self.pw1.place(x=540, y=265, width=210, height=27)
+        self.pw1.place(x=530, y=265, width=240, height=27)
 
-        self.sign_up = tk.Button(text="Sign Up", font=buttonFont, command=lambda: template.changePage(self, "sign up"))
+        self.sign_up = tk.Button(text="Sign Up", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: template.changePage(self, "sign up"))
         self.sign_up.place(x=420, y=350, width=90)
 
-        self.forget_password = tk.Button(text="Forget Password", font=buttonFont, command=lambda: template.changePage(self, "forget password 1"))
-        self.forget_password.place(x=750, y=350, width=150)
+        self.forget_password = tk.Button(text="Forget Password", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: template.changePage(self, "forget password 1"))
+        self.forget_password.place(x=740, y=350, width=170)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.isSignInValid())
+        self.submit = tk.Button(text="Submit", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.isSignInValid())
         self.submit.place(x=597, y=470, width=100, height=40)
 
     def isSignInValid(self):
@@ -53,7 +53,7 @@ class signIn(tk.Frame):
         pw = self.pw1.get()
 
         if loginData == "" or pw == "":
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
+            tkMessageBox.showwarning("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
             return
 
         if loginData in listEmail or loginData in listUsername:
@@ -65,13 +65,14 @@ class signIn(tk.Frame):
             if listPass[idx] == pw:
                 template.changePage(self, "film")
             else:
-                tkMessageBox.showinfo("Netfl\'IXX\'", "Email/Username tidak sesuai dengan Password")
+                tkMessageBox.showerror("Netfl\'IXX\'", "Email/Username tidak sesuai dengan Password")
         else:
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Email/Username tidak valid")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Email/Username tidak terdaftar")
 
 def startPage():
     root = tk.Tk()
-    root.title("Testing")
+    root.title("Netfl\'IXX\'")
     root.geometry("1920x1080")
+    root.configure(bg="#24225e")
     app = signIn(master = root)
     app.mainloop()
