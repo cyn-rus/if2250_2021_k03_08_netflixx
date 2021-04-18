f9e19f02468cc7eb3bbceaa41e1e16e942c7e8af
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-19 00:28:21 +0700

    debug forget pw

--- src/user/forgetPassword.py
+++ src/user/forgetPassword.py
@@ -3,7 +3,7 @@ import tkinter.font as tkFont
 import tkinter.messagebox as tkMessageBox
 import yagmail
 import random
-import signUp.forgetPassword as forgetPassword
+from user.signUp import checkPassword
 import sys
 sys.path.insert(1, "..")
 import template
@@ -51,27 +51,27 @@ class forgetPassword1(tk.Frame):
         
         try:
             generatedCode = sendEmail(listNama[idx], listEmail[idx])
-            template.changePage("forget password 2")
-            self.goToNextPage(generatedCode, listEmail[idx])
+            # template.changePage("forget password 2")
+            self.goToNextPage(generatedCode, idx)
         except:
             return
 
-    def goToNextPage(self, code):
-        self.destroy()
+    def goToNextPage(self, code, idx):
+        self.master.destroy()
         root = tk.Tk()
         root.title("Testing")
         root.geometry("1920x1080")
-        app = forgetPassword2(code, email, master=root)
+        app = forgetPassword2(code, idx, master=root)
         app.mainloop()
     
 class forgetPassword2(tk.Frame):
-    def __init__(self, code, email, master=None):
+    def __init__(self, code, idx, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.forget_password2(code, email)
+        self.forget_password2(code, idx)
     
-    def forget_password2(self, code, email):
+    def forget_password2(self, code, idx):
         template.header(" forget  password")
         template.button_halaman_utama(self)
 
@@ -88,56 +88,54 @@ class forgetPassword2(tk.Frame):
         self.pin1 = tk.Entry(font=entryFont)
         self.pin1.place(x=580, y=275, width=100, height=30)
 
-        self.resend = tk.Button(text="Kirim Ulang", font=buttonFont, command=lambda: self.reSendEmail(email))
+        self.resend = tk.Button(text="Kirim Ulang", font=buttonFont, command=lambda: self.reSendEmail(idx))
         self.resend.place(x=400, y=400, width=120, height=43)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.validateCode(email, code))
+        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.validateCode(idx, code))
         self.submit.place(x=740, y=400, width=90, height=43)
 
-    def reSendEmail(self, email):
+    def reSendEmail(self, idx):
         data = template.readFile("user.csv")
         listNama = [row[2] for row in data]
         listEmail = [row[0] for row in data]
         
-        idx = listEmail.index(email)
-
         try:
             generatedCode = sendEmail(listNama[idx], listEmail[idx])
-            self.forget_password2(generatedCode, listEmail[idx])
+            self.forget_password2(generatedCode, idx)
         except:
             return
 
-    def validateCode(self, email, code):
+    def validateCode(self, idx, code):
         inputCode = self.pin1.get()
 
         if inputCode == "":
             tkMessageBox.showinfo("Netfl\'IXX\'", "Dimohonkan untuk mengisi pinnya")
             return
         
-        if inputCode == code:
-            self.goToNextPage(email)
+        if inputCode == str(code):
+            self.goToNextPage(idx)
         else:
             textFont = tkFont.Font(size=15)
             self.wrongPin = tk.Label(text="Pin yang dimasukkan tidak sesuai", font=textFont)
-            self.wrongPin.place(x=500, y=450)
+            self.wrongPin.place(x=480, y=450)
             self.after(2000, self.wrongPin.destroy)
 
-    def goToNextPage(self, email):
-        self.destroy()
+    def goToNextPage(self, idx):
+        self.master.destroy()
         root = tk.Tk()
         root.title("Testing")
         root.geometry("1920x1080")
-        app = forgetPassword3(email, master=root)
+        app = forgetPassword3(idx, master=root)
         app.mainloop()
 
 class forgetPassword3(tk.Frame):
-    def __init__(self, email, master=None):
+    def __init__(self, idx, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.forget_password3(email)
+        self.forget_password3(idx)
     
-    def forget_password3(self, email):
+    def forget_password3(self, idx):
         template.header(" forget  password")
         template.button_halaman_utama(self)
         
@@ -155,24 +153,29 @@ class forgetPassword3(tk.Frame):
         self.conf_pw1 = tk.Entry(font=entryFont, show="*")
         self.conf_pw1.place(x=527, y=330, width=210, height=27)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont)
+        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.changePassword(idx))
         self.submit.place(x=588, y=430, width=90, height=43)
 
-    def changePassword(self, email):
+    def changePassword(self, idx):
         pw = self.pw1.get()
         confPw = self.conf_pw1.get()
 
-        if not forgetPassword(pw, confPw):
+        if pw == "" or confPw == "":
+            tkMessageBox.showinfo("Dimohonkan duntuk")
+            return
+
+        if not checkPassword(pw, confPw):
             return
 
         tkMessageBox.showinfo("Netfl\'IXX\'", "Password berhasil diubah")
         data = template.readFile("user.csv")
 
         listEmail = [data[0] for row in data]
-        idx = listEmail.index(email)
-        data[3][idx] = pw
+
+        print(idx, type(idx))
+        data[idx][3] = pw
         template.reWriteFile(data, "user.csv")
-        template.changePage("halaman utama")
+        template.changePage(self, "halaman utama")
 
 def sendEmail(receiverName, receiverEmail):
     RNGcode = random.randint(10**5, (10**6)-1)
@@ -185,8 +188,9 @@ def sendEmail(receiverName, receiverEmail):
     sender = "donotreply.net.flixx@gmail.com"
     senderPW = "ixzwsyxuzsevoloz"
 
-    yag = yagmail.SMTP(user=sender, password=senderPW)
-    yag.send(to=receiverEmail, subject="Netfl\'IXX\' Reset Password Request", contents=text)
+    print(RNGcode)
+    # yag = yagmail.SMTP(user=sender, password=senderPW)
+    # yag.send(to=receiverEmail, subject="Netfl\'IXX\' Reset Password Request", contents=text)
     return RNGcode
 
 def startPage():
