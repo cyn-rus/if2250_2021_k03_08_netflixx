3ddc267957c5970aca540ed8783747b77f6cf46b
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-19 02:09:16 +0700

    better gui forget password

--- src/user/forgetPassword.py
+++ src/user/forgetPassword.py
@@ -16,19 +16,19 @@ class forgetPassword1(tk.Frame):
         self.forget_password()
 
     def forget_password(self):
-        template.header(" forget  password")
+        template.header("   forget  password")
         template.button_halaman_utama(self)
 
-        textFont = tkFont.Font(size=15)
+        textFont = tkFont.Font(family="TimeBurner", size=15, weight="bold")
         entryFont = tkFont.Font(size=13)
-        buttonFont = tkFont.Font(size=15)
+        buttonFont = tkFont.Font(family="TimeBurner", size=15, weight="bold")
 
-        self.email = tk.Label(text="Masukkan Email/Username", font=textFont)
+        self.email = tk.Label(text="Masukkan Email/Username", font=textFont, bg="#24225e", fg="#9f64d8")
         self.email.place(x=522, y=200)
         self.email1 = tk.Entry(font=entryFont)
         self.email1.place(x=488, y=235, width=300, height=30)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.sendEmail())
+        self.submit = tk.Button(text="Submit", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.sendEmail())
         self.submit.place(x=597, y=400, width=100, height=45)
 
     def sendEmail(self):
@@ -40,13 +40,13 @@ class forgetPassword1(tk.Frame):
         email = self.email1.get()
         
         if email == "":
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
+            tkMessageBox.showwarning("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
             return
         
         try:
             idx = listEmail.index(email)
         except:
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Email tidak terdaftar")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Email tidak terdaftar")
             return
         
         try:
@@ -61,6 +61,7 @@ class forgetPassword1(tk.Frame):
         root = tk.Tk()
         root.title("Testing")
         root.geometry("1920x1080")
+        root.configure(bg="#24225e")
         app = forgetPassword2(code, idx, master=root)
         app.mainloop()
     
@@ -72,26 +73,27 @@ class forgetPassword2(tk.Frame):
         self.forget_password2(code, idx)
     
     def forget_password2(self, code, idx):
-        template.header(" forget  password")
+        template.header("   forget  password")
         template.button_halaman_utama(self)
 
-        textFont = tkFont.Font(size=15)
+        textFont = tkFont.Font(family="TimeBurner", size=15, weight="bold")
+        popupFont = tkFont.Font(family="TimeBurner", size=17, weight="bold")
         entryFont = tkFont.Font(size=13)
-        buttonFont = tkFont.Font(size=13)
+        buttonFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
 
-        self.link = tk.Label(text="Kode reset password sudah dikirimkan melalui email", font=textFont)
+        self.link = tk.Label(text="Kode reset password sudah dikirimkan melalui email", font=popupFont, bg="#24225e", fg="#9f64d8")
         self.link.place(x=400, y=100)
         self.after(2000, self.link.destroy)
 
-        self.pin = tk.Label(text="Masukkan pin yang telah dikirim", font=textFont)
+        self.pin = tk.Label(text="Masukkan pin yang telah dikirim", font=textFont, bg="#24225e", fg="#9f64d8")
         self.pin.place(x=491, y=239)
         self.pin1 = tk.Entry(font=entryFont)
         self.pin1.place(x=580, y=275, width=100, height=30)
 
-        self.resend = tk.Button(text="Kirim Ulang", font=buttonFont, command=lambda: self.reSendEmail(idx))
+        self.resend = tk.Button(text="Kirim Ulang", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.reSendEmail(idx))
         self.resend.place(x=400, y=400, width=120, height=43)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.validateCode(idx, code))
+        self.submit = tk.Button(text="Submit", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.validateCode(idx, code))
         self.submit.place(x=740, y=400, width=90, height=43)
 
     def reSendEmail(self, idx):
@@ -109,22 +111,20 @@ class forgetPassword2(tk.Frame):
         inputCode = self.pin1.get()
 
         if inputCode == "":
-            tkMessageBox.showinfo("Netfl\'IXX\'", "Dimohonkan untuk mengisi pinnya")
+            tkMessageBox.showwarning("Netfl\'IXX\'", "Dimohonkan untuk mengisi pinnya")
             return
         
         if inputCode == str(code):
             self.goToNextPage(idx)
         else:
-            textFont = tkFont.Font(size=15)
-            self.wrongPin = tk.Label(text="Pin yang dimasukkan tidak sesuai", font=textFont)
-            self.wrongPin.place(x=480, y=450)
-            self.after(2000, self.wrongPin.destroy)
+            tkMessageBox.showerror("Netfl\'IXX\'", "Pin tidak sesuai")
 
     def goToNextPage(self, idx):
         self.master.destroy()
         root = tk.Tk()
         root.title("Testing")
         root.geometry("1920x1080")
+        root.configure(bg="#24225e")
         app = forgetPassword3(idx, master=root)
         app.mainloop()
 
@@ -136,24 +136,24 @@ class forgetPassword3(tk.Frame):
         self.forget_password3(idx)
     
     def forget_password3(self, idx):
-        template.header(" forget  password")
+        template.header("   forget  password")
         template.button_halaman_utama(self)
         
-        textFont = tkFont.Font(size=15)
+        textFont = tkFont.Font(family="TimeBurner", size=15, weight="bold")
         entryFont = tkFont.Font(size=13)
-        buttonFont = tkFont.Font(size=13)
+        buttonFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
 
-        self.pw = tk.Label(text="Password Baru", font=textFont)
+        self.pw = tk.Label(text="Password Baru", font=textFont, bg="#24225e", fg="#9f64d8")
         self.pw.place(x=563, y=200)
         self.pw1 = tk.Entry(font=entryFont, show="*")
         self.pw1.place(x=527, y=230, width=210, height=27)
 
-        self.conf_pw = tk.Label(text="Konfirmasi Password Baru", font=textFont)
+        self.conf_pw = tk.Label(text="Konfirmasi Password Baru", font=textFont, bg="#24225e", fg="#9f64d8")
         self.conf_pw.place(x=510, y=300)
         self.conf_pw1 = tk.Entry(font=entryFont, show="*")
         self.conf_pw1.place(x=527, y=330, width=210, height=27)
 
-        self.submit = tk.Button(text="Submit", font=buttonFont, command=lambda: self.changePassword(idx))
+        self.submit = tk.Button(text="Submit", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.changePassword(idx))
         self.submit.place(x=588, y=430, width=90, height=43)
 
     def changePassword(self, idx):
@@ -161,7 +161,7 @@ class forgetPassword3(tk.Frame):
         confPw = self.conf_pw1.get()
 
         if pw == "" or confPw == "":
-            tkMessageBox.showinfo("Dimohonkan duntuk")
+            tkMessageBox.showwarning("Netfl\'IXX\'", "Dimohonkan untuk mengisi semua data")
             return
 
         if not checkPassword(pw, confPw):
@@ -197,5 +197,6 @@ def startPage():
     root = tk.Tk()
     root.title("Testing")
     root.geometry("1920x1080")
+    root.configure(bg="#24225e")
     app = forgetPassword1(master=root)
     app.mainloop()
