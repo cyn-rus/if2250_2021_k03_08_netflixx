3bcc475ca99164f7147f1adfe6daf6cd05f68938
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-19 02:09:27 +0700

    better gui main page

--- src/user/mainPage.py
+++ src/user/mainPage.py
@@ -15,26 +15,21 @@ class mainPage(tk.Frame):
         template.header("halaman utama")
         template.button_film(self)
         template.button_snack(self)
-        fontStyle = tkFont.Font(family="TimeBurner", size = 13)
+        fontStyle = tkFont.Font(family="TimeBurner", size=15, weight="bold")
         
-        self.sign_in = tk.Button(text="Sign In", font=fontStyle, command=lambda: template.changePage(self, "sign in"))
-        self.sign_in.place(x=625, y=200, width=140, height=50, anchor="c")
+        self.sign_in = tk.Button(text="Sign In", font=fontStyle, bg="#010027", fg="#9f64d8", activebackground="#010027", activeforeground="#9f64d8", command=lambda: template.changePage(self, "sign in"))
+        self.sign_in.place(x=630, y=200, width=170, height=50, anchor="c")
 
-        self.sign_up = tk.Button(text="Sign Up", font=fontStyle, command=lambda: template.changePage(self, "sign up"))
-        self.sign_up.place(x=625, y=300, width=140, height=50, anchor="c")
+        self.sign_up = tk.Button(text="Sign Up", font=fontStyle, bg="#010027", fg="#9f64d8", activebackground="#010027", activeforeground="#9f64d8", command=lambda: template.changePage(self, "sign up"))
+        self.sign_up.place(x=630, y=300, width=170, height=50, anchor="c")
 
-        self.forget = tk.Button(text="Forget Password", font=fontStyle, command=lambda: template.changePage(self, "forget password"))
-        self.forget.place(x=625, y=400, width=140, height=50, anchor="c")
+        self.forget = tk.Button(text="Forget Password", font=fontStyle, bg="#010027", fg="#9f64d8", activebackground="#010027", activeforeground="#9f64d8", command=lambda: template.changePage(self, "forget password"))
+        self.forget.place(x=630, y=400, width=170, height=50, anchor="c")
 
 def startPage():
     root = tk.Tk()
-    root.title("Testing")
+    root.title("Netfl\'IXX\'")
     root.geometry("1920x1080")
+    root.configure(bg="#24225e")
     app = mainPage(master=root)
     app.mainloop()
-
-# root = tk.Tk()
-# root.title("Testing")
-# root.geometry("1920x1080")
-# app = mainPage(master=root)
-# app.mainloop()
