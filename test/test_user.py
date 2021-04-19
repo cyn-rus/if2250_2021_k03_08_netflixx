941c10b0a35234d8edc7c9cdbd30be7524832ac4
cyn-rus <13519118@std.stei.itb.ac.id>
2021-04-19 17:56:35 +0700

    fix user testing

--- test/test_user.py
+++ test/test_user.py
@@ -2,9 +2,9 @@ import pytest
 import re
 
 data=[
-    ["a@gmail.com", "aaa", "ini a", "aAaA", "01/01/2001"],
-    ["b@gmail.com", "bbb", "bukan b", "lalala", "02/02/2002"],
-    ["c@gmail.com", "ccc", "saya c", "hemhemhem", "03/03/2003"]
+    ["a@gmail.com", "aaa", "ini a", "aAaA", "01/01/2001", "user"],
+    ["b@gmail.com", "bbb", "bukan b", "lalala", "02/02/2002", "admin"],
+    ["c@gmail.com", "ccc", "saya c", "hemhemhem", "03/03/2003", "user"]
     ]
 
 def isSignInValid(loginData, pw):
@@ -22,7 +22,7 @@ def isSignInValid(loginData, pw):
             idx = listUsername.index(loginData)
         
         if listPass[idx] == pw:
-            return True
+            return data[idx][5]
         else:
             return False
     else:
@@ -80,7 +80,7 @@ def isSignUpValid(email, username, pw, conf_pw, name, dob):
 
     alteredDOB = formatDOB[1] + "/" + formatDOB[0] + "/" + formatDOB[2]
 
-    return [email, username, name, pw, alteredDOB]
+    return [email, username, name, pw, alteredDOB, "user"]
 
 def sendEmail(email):
     listEmail = [row[0] for row in data]
@@ -127,8 +127,8 @@ def test():
     assert isSignInValid("d@gmail.com", "lalala") == False
     assert isSignInValid("b@gmail.com", "hemhemhem") == False
     assert isSignInValid("c@gmail.com", "apaya") == False
-    assert isSignInValid("a@gmail.com", "aAaA") == True
-    assert isSignInValid("bbb", "lalala") == True
+    assert isSignInValid("a@gmail.com", "aAaA") == "user"
+    assert isSignInValid("bbb", "lalala") == "admin"
 
     assert checkPassword("HaHaHa", "HaHaHa") == False
     assert checkPassword("Abcdefghi1!", "abcedfghi1!") == False
@@ -139,7 +139,7 @@ def test():
     assert isSignUpValid("a@gmail.com", "lala", "LaLaLaLa3#", "LaLaLa3#", "waaa", "1/1/01") == False
     assert isSignUpValid("d@gmail.com", " ", "LaLaLaLa3#", "LaLaLa3#", "waaa", "1/1/01") == False
     assert isSignUpValid("d@gmail.com", "aaa", "LaLaLaLa3#", "LaLaLaLa3#", "waaa", "1/1/01") == False
-    assert isSignUpValid("d@gmail.com", "ddd", "LaLaLaLa3#", "LaLaLaLa3#", "waaa", "1/1/01") == ["d@gmail.com", "ddd", "waaa", "LaLaLaLa3#", "01/01/2001"]
+    assert isSignUpValid("d@gmail.com", "ddd", "LaLaLaLa3#", "LaLaLaLa3#", "waaa", "1/1/01") == ["d@gmail.com", "ddd", "waaa", "LaLaLaLa3#", "01/01/2001", "user"]
 
     assert sendEmail("") == False
     assert sendEmail("huh@gmail.com") == False
