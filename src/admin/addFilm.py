7c140f7646e345d39119f37799f514ca4c9279af
Siti Iedrania <13519137@std.stei.itb.ac.id>
2021-04-20 15:12:18 +0700

    Fixed text vs entry

--- src/admin/addFilm.py
+++ src/admin/addFilm.py
@@ -11,8 +11,6 @@ class addFilm(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
-        #scroll_bar = tk.Scrollbar(self.master)
-        #scroll_bar.pack(side = tk.RIGHT, fill = tk.Y)
         self.add_film_page()
 
     def add_film_page(self):
@@ -33,11 +31,6 @@ class addFilm(tk.Frame):
         self.judul1 = tk.Entry(font=entryFont)
         self.judul1.place(x=270, y=165, width=210, height=27)
 
-        self.poster = tk.Label(text="Nama File Poster Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.poster.place(x=270, y=210)
-        self.poster1 = tk.Entry(font=entryFont)
-        self.poster1.place(x=270, y=235, width=210, height=27)
-
         self.tahun = tk.Label(text="Tahun Rilis Film", font=textFont, bg="#24225e", fg="#9f64d8")
         self.tahun.place(x=270, y=280)
         self.tahun1 = tk.Entry(font=entryFont)
@@ -53,18 +46,23 @@ class addFilm(tk.Frame):
         self.harga1 = tk.Entry(font=entryFont)
         self.harga1.place(x=270, y=445, width=210, height=27)
 
-        self.deskripsi = tk.Label(text="Deskripsi Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.deskripsi.place(x=270, y=490)
-        self.deskripsi1 = tk.Entry(font=entryFont)
-        self.deskripsi1.place(x=270, y=515, width=840, height=27)
+        self.poster = tk.Label(text="Nama File Poster Film", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.poster.place(x=270, y=210)
+        self.poster1 = tk.Entry(font=entryFont)
+        self.poster1.place(x=270, y=235, width=210, height=27)
 
-        self.film = tk.Label(text="Teks Isi Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.film.place(x=270, y=560)
+        self.film = tk.Label(text="Nama File Film", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.film.place(x=270, y=490)
         self.film1 = tk.Entry(font=entryFont)
-        self.film1.place(x=270, y=585, width=840, height=27)
+        self.film1.place(x=270, y=515, width=210, height=27)
+
+        self.deskripsi = tk.Label(text="Deskripsi Film", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.deskripsi.place(x=270, y=560)
+        self.deskripsi1 = tk.Text(font=entryFont, wrap=tk.WORD)
+        self.deskripsi1.place(x=270, y=585, width=840, height=42)
 
         self.submit = tk.Button(text="Add Film", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.isAddFilmValid())
-        self.submit.place(x=590, y=660, width=140, height=40)
+        self.submit.place(x=590, y=650, width=140, height=40)
 
     def isAddFilmValid(self):
         data = template.readFile("film.csv")
@@ -76,11 +74,11 @@ class addFilm(tk.Frame):
         tahun = self.tahun1.get()
         umur = self.umur1.get()
         harga = self.harga1.get()
-        deskripsi = self.deskripsi1.get()
+        deskripsi = self.deskripsi1.get("1.0", "end-1c")
         film = self.film1.get()
 
         if id == "" or judul == "" or poster == "" or tahun == "" or umur == "" or harga == "" or deskripsi == "" or film == "":
-            tkMessageBox.showwarning("Netfl\'IXX\'", "Semua data film harus diisi")
+            tkMessageBox.showwarning("Netfl\'IXX\'", "Semua atribut film harus diisi")
             return
         
         if " " in id:
@@ -98,7 +96,7 @@ class addFilm(tk.Frame):
             tkMessageBox.showerror("Netfl\'IXX\'", "Batas umur penonton harus berupa angka")
             return
         if not re.search(numberRegex, harga):
-            tkMessageBox.showerror("Netfl\'IXX\'", "Harga film harus berupa angka")
+            tkMessageBox.showerror("Netfl\'IXX\'", "Harga tiket film harus berupa angka")
             return
 
         template.writeFile([id, judul, poster, tahun, 0, umur, deskripsi, 0, harga, film], "film.csv")
@@ -109,7 +107,7 @@ class addFilm(tk.Frame):
         self.tahun1.delete(0, tk.END)
         self.umur1.delete(0, tk.END)
         self.harga1.delete(0, tk.END)
-        self.deskripsi1.delete(0, tk.END)
+        self.deskripsi1.delete("1.0", tk.END)
         self.film1.delete(0, tk.END)
 
 def startPage():
--- src/admin/updateFilm.py
+++ src/admin/updateFilm.py
@@ -25,24 +25,27 @@ class updateFilm(tk.Frame):
         buttonFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
 
         self.id = tk.Label(text="ID Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.id.place(x=270, y=70)
+        self.id.place(x=140, y=70)
         self.id1 = tk.Entry(font=entryFont)
-        self.id1.place(x=270, y=95, width=210, height=27)
+        self.id1.place(x=140, y=95, width=210, height=27)
 
         self.submit = tk.Button(text="Pilih Film", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.isIDFilmValid())
         self.submit.place(x=800, y=75, width=140, height=40)
     
     def isIDFilmValid(self):
+        data = template.readFile("film.csv")
+        idFilm = [row[0] for row in data]
+        id = self.id1.get()
+
+        if id not in idFilm:
+            tkMessageBox.showwarning("Netfl\'IXX\'", "ID film tidak ditemukan")
+            return
+        
         textFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
+        textFont2 = tkFont.Font(family="TimeBurner", size=13, weight="bold")
         entryFont = tkFont.Font(size=13)
         buttonFont = tkFont.Font(family="TimeBurner", size=14, weight="bold")
 
-        self.submit.destroy()
-        self.back = tk.Button(text="Ubah ID Film", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: template.changePage(self, "update film"))
-        self.back.place(x=800, y=75, width=140, height=40)
-
-        data = template.readFile("film.csv")
-        idFilm = [row[0] for row in data]
         judulFilm = [row[1] for row in data]
         posterFilm = [row[2] for row in data]
         tahunFilm = [row[3] for row in data]
@@ -50,93 +53,90 @@ class updateFilm(tk.Frame):
         deskripsiFilm = [row[6] for row in data]
         hargaFilm = [row[8] for row in data]
         filmFilm = [row[9] for row in data]
-
-        id = self.id1.get()
-
-        if id not in idFilm:
-            tkMessageBox.showwarning("Netfl\'IXX\'", "ID film tidak ditemukan")
-            return
-
         i = idFilm.index(id)
-
+        
+        self.submit.destroy()
+        self.back = tk.Button(text="Cari Film Lain", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: template.changePage(self, "update film"))
+        self.back.place(x=800, y=75, width=140, height=40)
+        
         # label
         self.id1.destroy()
-        self.id1 = tk.Label(text=id, font=textFont, bg="#24225e", fg="#9f64d8")
-        self.id1.place(x=270, y=95)
+        self.id1 = tk.Label(text=id, font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.id1.place(x=140, y=95)
 
         self.judul = tk.Label(text="Judul Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.judul.place(x=270, y=140)
-        self.judul1 = tk.Label(text=judulFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.judul1.place(x=270, y=165)
+        self.judul.place(x=140, y=140)
+        self.judul1 = tk.Label(text=judulFilm[i], font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.judul1.place(x=140, y=165)
 
         self.poster = tk.Label(text="Nama File Poster Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.poster.place(x=270, y=210)
-        self.poster1 = tk.Label(text=posterFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.poster1.place(x=270, y=235)
+        self.poster.place(x=140, y=210)
+        self.poster1 = tk.Label(text=posterFilm[i], font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.poster1.place(x=140, y=235)
 
         self.tahun = tk.Label(text="Tahun Rilis Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.tahun.place(x=270, y=280)
-        self.tahun1 = tk.Label(text=tahunFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.tahun1.place(x=270, y=305)
+        self.tahun.place(x=140, y=280)
+        self.tahun1 = tk.Label(text=tahunFilm[i], font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.tahun1.place(x=140, y=305)
 
         self.umur = tk.Label(text="Batas Umur Penonton", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.umur.place(x=270, y=350)
-        self.umur1 = tk.Label(text=umurFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.umur1.place(x=270, y=375)
+        self.umur.place(x=140, y=350)
+        self.umur1 = tk.Label(text=umurFilm[i], font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.umur1.place(x=140, y=375)
 
         self.harga = tk.Label(text="Harga Tiket Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.harga.place(x=270, y=420)
-        self.harga1 = tk.Label(text=hargaFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.harga1.place(x=270, y=445)
+        self.harga.place(x=140, y=420)
+        self.harga1 = tk.Label(text=hargaFilm[i], font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.harga1.place(x=140, y=445)
 
         self.deskripsi = tk.Label(text="Deskripsi Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.deskripsi.place(x=270, y=490)
-        self.deskripsi1 = tk.Label(text=deskripsiFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.deskripsi1.place(x=270, y=515)
+        self.deskripsi.place(x=140, y=490)
+        self.deskripsi1 = tk.Label(text=deskripsiFilm[i][0:50]+"...", font=textFont2, bg="#24225e", fg="#9f64d8", wraplength=500, justify=tk.LEFT)
+        self.deskripsi1.place(x=140, y=515)
 
         self.film = tk.Label(text="Teks Isi Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.film.place(x=270, y=560)
-        self.film1 = tk.Label(text=filmFilm[i], font=textFont, bg="#24225e", fg="#9f64d8")
-        self.film1.place(x=270, y=585)
+        self.film.place(x=140, y=560)
+        self.film1 = tk.Label(text=filmFilm[i], font=textFont2, bg="#24225e", fg="#9f64d8")
+        self.film1.place(x=140, y=585)
 
         # entry
         self.njudul = tk.Label(text="Judul Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.njudul.place(x=800, y=140)
+        self.njudul.place(x=670, y=140)
         self.njudul1 = tk.Entry(font=entryFont)
-        self.njudul1.place(x=800, y=165, width=210, height=27)
+        self.njudul1.place(x=670, y=165, width=210, height=27)
 
         self.nposter = tk.Label(text="Nama File Poster Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.nposter.place(x=800, y=210)
+        self.nposter.place(x=670, y=210)
         self.nposter1 = tk.Entry(font=entryFont)
-        self.nposter1.place(x=800, y=235, width=210, height=27)
+        self.nposter1.place(x=670, y=235, width=210, height=27)
 
         self.ntahun = tk.Label(text="Tahun Rilis Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.ntahun.place(x=800, y=280)
+        self.ntahun.place(x=670, y=280)
         self.ntahun1 = tk.Entry(font=entryFont)
-        self.ntahun1.place(x=800, y=305, width=210, height=27)
+        self.ntahun1.place(x=670, y=305, width=210, height=27)
 
         self.numur = tk.Label(text="Batas Umur Penonton", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.numur.place(x=800, y=350)
+        self.numur.place(x=670, y=350)
         self.numur1 = tk.Entry(font=entryFont)
-        self.numur1.place(x=800, y=375, width=210, height=27)
+        self.numur1.place(x=670, y=375, width=210, height=27)
 
         self.nharga = tk.Label(text="Harga Tiket Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.nharga.place(x=800, y=420)
+        self.nharga.place(x=670, y=420)
         self.nharga1 = tk.Entry(font=entryFont)
-        self.nharga1.place(x=800, y=445, width=210, height=27)
+        self.nharga1.place(x=670, y=445, width=210, height=27)
 
         self.ndeskripsi = tk.Label(text="Deskripsi Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.ndeskripsi.place(x=800, y=490)
+        self.ndeskripsi.place(x=670, y=490)
         self.ndeskripsi1 = tk.Entry(font=entryFont)
-        self.ndeskripsi1.place(x=800, y=515, width=540, height=27)
+        self.ndeskripsi1.place(x=670, y=515, width=540, height=27)
 
-        self.nfilm = tk.Label(text="Teks Isi Film", font=textFont, bg="#24225e", fg="#9f64d8")
-        self.nfilm.place(x=800, y=560)
+        self.nfilm = tk.Label(text="Nama File Film", font=textFont, bg="#24225e", fg="#9f64d8")
+        self.nfilm.place(x=670, y=560)
         self.nfilm1 = tk.Entry(font=entryFont)
-        self.nfilm1.place(x=800, y=585, width=540, height=27)
+        self.nfilm1.place(x=670, y=585, width=540, height=27)
 
         self.submit = tk.Button(text="Update Film", font=buttonFont, bg="#010109", fg="#9f64d8", activebackground="#010109", activeforeground="#9f64d8", command=lambda: self.isUpdateFilmValid())
-        self.submit.place(x=590, y=660, width=140, height=40)
+        self.submit.place(x=590, y=650, width=140, height=40)
     
     def isUpdateFilmValid(self):
         data = template.readFile("film.csv")
